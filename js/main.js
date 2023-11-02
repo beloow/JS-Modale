dc046eb817e2e4912db0ce5717def44d9546f692
beloow <quevaqueva11@gmail.com>
2023-11-02 16:41:53 +0100

    Modale fini

--- js/main.js
+++ js/main.js
@@ -2,23 +2,22 @@
 const firstSection = document.getElementById('first');
 
 const bodyTag = firstSection.parentElement;
-console.log(bodyTag);
 
 bodyTag.insertAdjacentHTML('beforeend',"<main></main>");
 
 // Récupéré la modale
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
 // on rédupère le <span> du bouton X 
 var btn = document.getElementsByClassName("close")[0];
 
 // on fait apparaitre la modale au chargement
-window.onload = function() {
+window.onload = function modalopen() {
   const insertModale = `
     <div id="myModal" class="modal">
       <!-- Modal content -->
       <div class="modal-content">
-          <button class="close" onclick="close(event)"> <i class="fa-solid fa-x"></i> </button>
+          <button class="close" id="close" onclick='closeModal'> <i class="fa-solid fa-x"></i> </button>
 
           <h1>Titre du modale</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus fuga est aspernatur natus, doloribus numquam.</p>
@@ -28,18 +27,17 @@ window.onload = function() {
   bodyTag.insertAdjacentHTML('beforeend', insertModale);
 }
 
-// Quand l'utilisateur clique sur le "x" la modale disparait
-
-onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+const openModal = () => {
+  const modal = document.getElementById("myModal");
+  modal.style.display = "block";
 }
 
-// faire en sorte pour que lorsque l'utilisateur clique ailleurs la modale se ferme
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+const closeModal = () => {
+  const modal = document.getElementById("myModal");
+  modal.style.display = "none";
 }
+
+window.addEventListener('click', (e) => {
+  const modal = document.getElementById("myModal");
+  modal.style.display = "none";
+})
