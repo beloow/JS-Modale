cb07b4fba2c87758f866db0b5e9906b2ee0035ff
beloow <quevaqueva11@gmail.com>
2023-11-02 12:34:46 +0100

    update

--- js/main.js
+++ js/main.js
@@ -5,23 +5,20 @@ const bodyTag = firstSection.parentElement;
 console.log(bodyTag);
 
 bodyTag.insertAdjacentHTML('beforeend',"<main></main>");
+
 // Récupéré la modale
 var modal = document.getElementById("myModal");
 
 // on rédupère le <span> du bouton X 
-var span = document.getElementsByClassName("close")[0];
+var btn = document.getElementsByClassName("close")[0];
 
 // on fait apparaitre la modale au chargement
 window.onload = function() {
-  // modal.style.display = "block";
-  // modal.style.opacity = "1";
-
   const insertModale = `
     <div id="myModal" class="modal">
       <!-- Modal content -->
       <div class="modal-content">
-          <span class="close">&times;</span>
-          <img src="http://worldpoint.com.br/media/wysiwyg/banner/banner-recesso-2019.jpeg" alt="">
+          <button class="close" onclick="close(event)"> <i class="fa-solid fa-x"></i> </button>
 
           <h1>Titre du modale</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus fuga est aspernatur natus, doloribus numquam.</p>
@@ -32,11 +29,15 @@ window.onload = function() {
 }
 
 // Quand l'utilisateur clique sur le "x" la modale disparait
-span.onclick = function() {
-  modal.style.display = "none";
+
+onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 // faire en sorte pour que lorsque l'utilisateur clique ailleurs la modale se ferme
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
