7506772a6f8169e57a0ba15782d4d9803f002ee7
beloow <quevaqueva11@gmail.com>
2023-11-02 10:03:42 +0100

    all Js
    maintenant la modale viens avec le JS est par default n'est pas dans l'html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
+// localisation du main 
+const firstSection = document.getElementById('first');
+
+const bodyTag = firstSection.parentElement;
+console.log(bodyTag);
+
+bodyTag.insertAdjacentHTML('beforeend',"<main></main>");
 // Récupéré la modale
 var modal = document.getElementById("myModal");
 
 // on rédupère le <span> du bouton X 
 var span = document.getElementsByClassName("close")[0];
 
-onload = function() {
-  modal.style.display = "block";
-  modal.style.opacity = "1";
+// on fait apparaitre la modale au chargement
+window.onload = function() {
+  // modal.style.display = "block";
+  // modal.style.opacity = "1";
+
+  const insertModale = `
+    <div id="myModal" class="modal">
+      <!-- Modal content -->
+      <div class="modal-content">
+          <span class="close">&times;</span>
+          <img src="http://worldpoint.com.br/media/wysiwyg/banner/banner-recesso-2019.jpeg" alt="">
+
+          <h1>Titre du modale</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus fuga est aspernatur natus, doloribus numquam.</p>
+      </div>
+    </div>
+  `;
+  bodyTag.insertAdjacentHTML('beforeend', insertModale);
 }
 
 // Quand l'utilisateur clique sur le "x" la modale disparait
@@ -14,7 +36,7 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// faire en sorte pour que lorsque l'utilisateur clique ailleurs la modale se ferme
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
