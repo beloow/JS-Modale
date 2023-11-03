d638ef9743e8a397129fa5b25d91f8c637246a42
beloow <quevaqueva11@gmail.com>
2023-11-03 14:27:52 +0100

    Update js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ const firstSection = document.getElementById('first');
 
 const bodyTag = firstSection.parentElement;
 
-bodyTag.insertAdjacentHTML('beforeend',"<main></main>");
-
 // Récupéré la modale
 const modal = document.getElementById("myModal");
 
@@ -12,12 +10,11 @@ const modal = document.getElementById("myModal");
 var btn = document.getElementsByClassName("close")[0];
 
 // on fait apparaitre la modale au chargement
-window.onload = function modalopen() {
+function modalOpen() {
   const insertModale = `
     <div id="myModal" class="modal">
-      <!-- Modal content -->
       <div class="modal-content">
-          <button class="close" id="close" onclick='closeModal'> <i class="fa-solid fa-x"></i> </button>
+          <button class="close" id="close"> <i class="fa-solid fa-x"></i> </button>
 
           <h1>Titre du modale</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus fuga est aspernatur natus, doloribus numquam.</p>
@@ -30,20 +27,53 @@ window.onload = function modalopen() {
   para.style.display = "none";
 }
 
-// const openModal = () => {
-//   const modal = document.getElementById("myModal");
-//   modal.style.display = "block";
-// }
+modalOpen();
 
+// const closemodal
 const closeModal = () => {
   const modal = document.getElementById("myModal");
-  modal.style.display = "none";
+
+  modal.remove();
 }
 
+// Au click sur la page, 1 la modale disparait, 2 le texte aparait
 window.addEventListener('click', (e) => {
   const modal = document.getElementById("myModal");
-  modal.style.display = "none";
-
   const para = document.getElementById("paragraphe");
-  para.style.display = "block";
+  if (e.target == modal) {
+    modal.remove();
+    para.style.display = "block";
+  }
 })
+
+const btHeader = document.getElementById('makeTitleRed');
+btHeader.addEventListener("click", redTitle);
+
+// Avec toggle enlever la class pour que sa revienne en noir
+
+function redTitle(e) {
+  const sectionTitles = document.getElementsByClassName('section-title')[0];
+  sectionTitles.classList.toggle('red');
+}
+
+// ///////////////  CSS  ///////////////
+const paraBgColor = "gray";
+
+const sectionSousTitles = document.querySelectorAll('main section h2');
+//console.log(sectionSousTitles);
+for (let sectionSousTitle of sectionSousTitles) {
+  sectionSousTitle.classList.add('section-sous-title');
+}
+
+const sectionTitles = document.querySelectorAll('main section h1');
+for (let sectionTitle of sectionTitles) {
+  sectionTitle.classList.add('section-title');
+}
+
+function styleparagraphe(paraBgColor) {
+  const paragraphe = document.getElementById("paragraphe");
+  paragraphe.style.display = "flex";
+  paragraphe.style.justifyContent = "center";
+  paragraphe.style.background = paraBgColor;
+  paragraphe.style.padding = "15px";
+}
